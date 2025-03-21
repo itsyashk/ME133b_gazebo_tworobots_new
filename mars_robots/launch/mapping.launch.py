076b223b40210f0215c140405b12bd1d3a0e0421
itsyashk <141369028+itsyashk@users.noreply.github.com>
2025-03-20 23:11:10 -0700

    Add mapping launch file for the robots

---
+++ mars_robots/launch/mapping.launch.py
@@ -0,0 +1,41 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    slam_params_file = LaunchConfiguration('slam_params_file')
+
+    pkg_share = get_package_share_directory('mars_robots')
+    default_params_file = os.path.join(pkg_share, 'config', 'slam_params.yaml')
+
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation/Gazebo clock')
+    
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=default_params_file,
+        description='Full path to the ROS2 parameters file to use for the SLAM toolbox node')
+
+    start_async_slam_toolbox_node = Node(
+        parameters=[
+          slam_params_file,
+          {'use_sim_time': use_sim_time}
+        ],
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen')
+
+    ld = LaunchDescription()
+
+    ld.add_action(declare_use_sim_time_argument)
+    ld.add_action(declare_slam_params_file_cmd)
+    ld.add_action(start_async_slam_toolbox_node)
+
+    return ld
